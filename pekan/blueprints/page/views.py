1e15f0bc519fc65bceec690de47d535d52284f25
imalkov82 <igor.malkov82@gmail.com>
2017-04-02 08:05:55 +0300

    use home page only

--- pekan/blueprints/page/views.py
+++ pekan/blueprints/page/views.py
@@ -8,11 +8,11 @@ def home():
     return render_template('page/home.html')
 
 
-@page.route('/terms')
-def terms():
-    return render_template('page/terms.html')
-
-
-@page.route('/privacy')
-def privacy():
-    return render_template('page/privacy.html')
+# @page.route('/terms')
+# def terms():
+#     return render_template('page/terms.html')
+#
+#
+# @page.route('/privacy')
+# def privacy():
+#     return render_template('page/privacy.html')
